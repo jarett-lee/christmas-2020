27ce6fab029c5315c5f1c2f740bc6ed47c8d3cb9
Jarett Lee <jarett.lee.pi@gmail.com>
2020-11-26 12:40:01 -0500

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ["react", "@typescript-eslint", "import"],
+  rules: {
+    "import/no-default-export": "error",
+  },
+  settings: {
+    react: {
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // default to latest and warns if missing
+      // It will default to "detect" in the future
+    },
+  },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+/* eslint import/no-default-export: "off" */
+
 import React from "react"
 import { Link } from "gatsby"
 
